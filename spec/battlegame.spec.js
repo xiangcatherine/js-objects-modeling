d8e2f7fca59423d4a6c0c556459498184f76ff92
MattB-GA-Boston <matt.brendzel@generalassemb.ly>
2015-06-25 23:25:51 -0400

    Fixes a bug in the spec
    I had forgotton to include `game.` before each of the unit types in the 'gameplay' section of the .spec file. Now fixed.

--- spec/battlegame.spec.js
+++ spec/battlegame.spec.js
@@ -5,47 +5,44 @@ var game = require('../lib/battlegame.js');
 describe('game units', function(){
   describe('infantry unit', function(){
     it('exists and has the correct properties', function(){
-      expect(game.infantry).not.toBe(undefined);
-      expect(game.infantry.health).not.toBe(undefined);
-      expect(game.infantry.speed).not.toBe(undefined);
+      expect(game.infantry).toBeDefined();
+      expect(game.infantry.health).toBeDefined();
+      expect(game.infantry.speed).toBeDefined();
     });
     it('has the values for its properties that are given in the spec', function(){
       expect(game.infantry.health).toBe(2);
       expect(game.infantry.speed).toBe(2);
     });
-    it('has an attack method, which takes another unit as an argument', function(){
-      expect(game.infantry.attack).not.toBe(undefined);
-      expect(typeof game.infantry.attack).toBe(function);
+    it('has an attack method', function(){
+      expect(game.infantry.attack).toBeDefined();
     });
   });
   describe('artillery unit', function(){
     it('exists and has the correct properties', function(){
-      expect(game.artilery).not.toBe(undefined);
-      expect(game.artilery.health).not.toBe(undefined);
-      expect(game.artilery.speed).not.toBe(undefined);
+      expect(game.artillery).toBeDefined();
+      expect(game.artillery.health).toBeDefined();
+      expect(game.artillery.speed).toBeDefined();
     });
     it('has the values for its properties that are given in the spec', function(){
-      expect(game.artilery.health).toBe(2);
-      expect(game.artilery.speed).toBe(2);
+      expect(game.artillery.health).toBe(2);
+      expect(game.artillery.speed).toBe(1);
     });
-    it('has an attack method, which takes another unit as an argument', function(){
-      expect(game.artilery.attack).not.toBe(undefined);
-      expect(typeof game.artilery.attack).toBe(function);
+    it('has an attack method', function(){
+      expect(game.artillery.attack).toBeDefined();
     });
   });
   describe('cavalry unit', function(){
     it('exists and has the correct properties', function(){
-      expect(game.cavalry).not.toBe(undefined);
-      expect(game.cavalry.health).not.toBe(undefined);
-      expect(game.cavalry.speed).not.toBe(undefined);
+      expect(game.cavalry).toBeDefined();
+      expect(game.cavalry.health).toBeDefined();
+      expect(game.cavalry.speed).toBeDefined();
     });
     it('has the values for its properties that are given in the spec', function(){
-      expect(game.cavalry.health).toBe(2);
-      expect(game.cavalry.speed).toBe(2);
+      expect(game.cavalry.health).toBe(1);
+      expect(game.cavalry.speed).toBe(3);
     });
-    it('has an attack method, which takes another unit as an argument', function(){
-      expect(game.cavalry.attack).not.toBe(undefined);
-      expect(typeof game.cavalry.attack).toBe(function);
+    it('has an attack method', function(){
+      expect(game.cavalry.attack).toBeDefined();
     });
   });
 });
@@ -53,20 +50,20 @@ describe('game units', function(){
 describe('gameplay', function(){
   describe('infantry attacks artillery', function(){
     it('decreases artillery health to 1', function(){
-      infantry.attack(artillery);
-      expect(artillery.health).toBe(1);
+      game.infantry.attack(game.artillery);
+      expect(game.artillery.health).toBe(1);
     });
   });
   describe('cavalry attacks infantry', function(){
     it('decreases infantry health to 1', function(){
-      cavalry.attack(infantry);
-      expect(infantry.health).toBe(1);
+      game.cavalry.attack(game.infantry);
+      expect(game.infantry.health).toBe(0);
     });
   });
   describe('artillery attacks cavalry', function(){
     it('decreases cavalry health to -2', function(){
-      artillery.attack(cavalry);
-      expect(cavalry.health).toBe(-2);
+      game.artillery.attack(game.cavalry);
+      expect(game.cavalry.health).toBe(-2);
     });
   });
 })
